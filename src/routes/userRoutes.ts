c534373076a995254660cceac9b76269c0707227
Michael Ruiz <michael@mruiz.dev>
2023-02-22 05:54:14 -0800

    Working on logout function

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import handleResponse from "../utils/handleResponse";
+const router = express.Router();
+const passport = require('passport');
+
+const successLoginUrl = "http://localhost:3000/login/success"
+const errorLoginUrl = "http://localhost:3000/login/error"
+
+
+router.get("/profile", (req: any, res, next) => {
+    if (req.user) {
+        res.send(req.user)
+        console.log(req.user)
+        // next();
+    } else {
+        res.status(401).send("You must login first!");
+    }
+})
+module.exports = router;
